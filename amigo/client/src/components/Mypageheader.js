106a997e13dd4d6bee1815e99bda640a2359e7aa
Hwanginyoung <hwang5375@gmail.com>
2022-10-26 17:27:14 +0900

    mypage

--- amigo/client/src/components/Mypageheader.js
+++ amigo/client/src/components/Mypageheader.js
@@ -7,7 +7,7 @@ function Mypageheader() {
     <div id="myheaderbox">
         <div id="myheader">
             <p>MY페이지에서<br/>회원정보를 확인하고 수정 할 수 있습니다.</p>
-            <button>로그아웃</button>
+            <button>회원탈퇴</button>
         </div>
     </div>
   );
